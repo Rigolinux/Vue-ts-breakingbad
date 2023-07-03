5cb850a1f23389ba2d27c3ed00c6f3d6aae51a1f
rigolinux <rigoberto7567@gmail.com>
2023-07-03 15:41:41 -0600

    script setup counter

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const counter = ref(0);
+const decrease = () => {
+      counter.value--;
+    };
+/* 
+old version requieres imports
+export default defineComponent({
+  name: 'App',
+  setup() {
+    
+    //methods
+    const decrease = () => {
+      counter.value--;
+    };
+
+    return {
+      counter,
+      decrease,
+    };
+  },
+}); */
 
 </script>
 
 <template>
   <div class="wrapper">
-    <h1>Hola mundo </h1>
+    <h1>Counter {{ counter }} </h1>
+    <button @click="counter++">Increment</button>
+    <button @click="decrease">Decrement</button>
   </div>
 </template>
 
